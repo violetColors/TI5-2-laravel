c84cf71384152706059783736abe95f9618310a7
violetColors <color4575@gmail.com>
2022-05-25 07:55:52 -0700

    hoy hice las condiciones de mayor de edad y menor de edad

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,40 @@
 |
 */
 
-$router->get('/', function () use ($router) {
+$router->get('/version', function () use ($router) {
     return $router->app->version();
+});
+
+$router->get('/saludo/{nombre}', function ($nombre) {
+    return 'Hola mi nombre es Violet' . $nombre;
+
+});
+$router->delete('/saludo', function(){
+    return "delete delete";
+
+});
+
+
+
+$router->get('/calcular/{edad}', function($edad){
+    if($edad>0 && $edad <18){
+        return 'eres menor de edad';
+    }elseif ($edad >= 18 && $edad <=100){
+        return 'eres mayor de edad';
+    }else{
+        return 'edad erronea';
+    }
+});
+
+
+
+
+
+$router->post('/saludo', function(){
+    return "post post";
+
+});
+$router->put('/saludo', function(){
+    return "get get";
+
 });
